7f13f4a2d4ac968cc549e24df02adaa5cf704195
afinocchiaro <alessandro.finocchiaro@telecomitalia.it>
2022-12-18 17:42:05 +0100

    fix: fix api for retrieve country by code

--- app/src/main/java/com/softdream/worldlocations/data/remote/LocationApiService.kt
+++ app/src/main/java/com/softdream/worldlocations/data/remote/LocationApiService.kt
@@ -1,17 +1,13 @@
 package com.softdream.worldlocations.data.remote
 
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface LocationApiService {
     @GET("/v3.1/all")
     suspend fun getLocations(): List<RemoteLocation>
 
-    @GET("/v3.1/alpha/{code}\"r_id\"")
-    suspend fun getLocationByCode(@Query("equalTo") code: String): List<RemoteLocation>
-
-    @GET("/unescosites.json?orderBy=\"r_id\"")
-    suspend fun getLocation(@Query("equalTo") id: Int): Map<String,RemoteLocation>
-
+    @GET("/v3.1/alpha/{code}")
+    suspend fun getLocationByCode(@Path("code") code: String): List<RemoteLocation>
 
 }
